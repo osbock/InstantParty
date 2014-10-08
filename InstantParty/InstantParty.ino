85fcd9ffe50a12efd20eef3e7923a01d1313e8a0
Kevin Osborn <kevin.osborn@gmail.com>
2014-10-08 12:50:32 -0400

    comment updates, LED indicator fix (arming)

--- InstantParty/InstantParty.ino
+++ InstantParty/InstantParty.ino
@@ -3,6 +3,11 @@
   and swtich on a power switch tail. 
   Derived from Sparkfun example: 
 https://learn.sparkfun.com/tutorials/mp3-player-shield-hookup/example-sketch-mp3-trigger
+
+NOTE:
+  Setup the SDCard with the MP3 Tracks renamed:
+  TRACK001.mp3, TRACK002.mp3, etc.
+  The play track function then uses the number (e.g integer 1 plays TRACK001.mp3
 */
 #include <SPI.h>           // SPI library
 #include <SdFat.h>         // SDFat Library
@@ -18,7 +23,7 @@ const uint8_t volume = 0; // MP3 Player volume 0=max, 255=lowest (off)
 const uint16_t monoMode = 0;  // Mono setting 0=off, 3=max
 
 int numtracks = 2;
-int trackplaying=0;
+int trackplaying=1;
 #define TIMETOPLAY 10000
 long time_elapsed; // keep track of how long the track has played.
 unsigned long time_started;
@@ -27,10 +32,11 @@ boolean triggerEnabled = false;
 #define MOTIONPIN A0
 #define POWERSWITCH A4
 #define BUTTONPIN A5
-#define LEDPIN A2
+#define LEDPIN A1
 void setup() {
   Serial.begin(115200);
   pinMode(POWERSWITCH,OUTPUT);
+  pinMode(LEDPIN,OUTPUT);
   digitalWrite(POWERSWITCH,LOW);
   pinMode(MOTIONPIN, INPUT);
   pinMode(BUTTONPIN,INPUT);
@@ -74,7 +80,9 @@ void loop() {
     Serial.println("motion detected");
     Serial.print("playing track: ");
     Serial.println(trackplaying);
-    trackplaying = trackplaying>=numtracks-1?0:trackplaying+1;
+
+    // Tracks go from 1-numtracks
+    trackplaying = trackplaying>=numtracks?1:trackplaying+1;
 
     time_started = millis();
     
